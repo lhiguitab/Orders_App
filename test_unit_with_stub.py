599c67b7a081a6a76f33fdf052e241ee40081fdc
lhiguitab <lhiguitab@eafit.edu.co>
2026-04-08 09:40:14 -0500

    Implementing unit testing with stubs

--- test_unit_with_stub.py
+++ test_unit_with_stub.py
@@ -5,7 +5,7 @@ from models import Order
 class StubUserRepository:
     def get_user_email(self, user_id):
         # TODO: retornar siempre el mismo email ficticio
-        return "_____________________"
+        return f"user{user_id}@example.com"
 
 class DummyLogger:
     def log(self, msg):
@@ -19,9 +19,9 @@ def test_create_order_with_stub():
     db = SessionLocal()
     order = create_order(10, 200, NullNotifier(), DummyLogger(), db, StubUserRepository())
     # TODO: Comparar con las respuestas esperadas
-    assert order.status == '______'
-    assert order.user_email == "_____________________"
-    
+    assert order.status == 'CREATED'
+    assert order.user_email == "user10@example.com"
+
     db.query(Order).delete()
     db.commit()
     db.close()
